07d7eff7f4b269c93109e6de78f9b0c04e8c2e5a
emrecanozkok <Em1991042639>
2021-11-08 20:51:54 +0300

    [fix] config fixed

--- src/config/config.go
+++ src/config/config.go
@@ -3,7 +3,7 @@ package config
 import "main/utils"
 
 var (
-	SERVICE_PORT         = utils.Env("SERVICE_PORT", "8889")
-	DUMP_DURATION 		 = utils.Env("RECORD_FREQ", "60")
-	LOG_PATH 		 	 = utils.Env("LOG_PATH", "/Users/emrecanozkok/projects/ys-odev/src/log")
+	SERVICE_PORT  = utils.Env("SERVICE_PORT", "8889")
+	DUMP_DURATION = utils.Env("RECORD_FREQ", "60")
+	LOG_PATH      = utils.Env("LOG_PATH", "/app/goys/log")
 )
